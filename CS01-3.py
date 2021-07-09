9d0774c13e03eb2db6522ebb929b0ebce9e11b0d
SisirunTongkobpetch <sisirun.tongkob@gmail.com>
2021-07-09 12:17:13 +0700

    update

---
+++ CS01-3.py
@@ -0,0 +1,7 @@
+A=int(input("Enter your Date of Birth:"))
+B=int(input("Enter your Birth Year:"))
+C=input("Enter your Month of Birth:",)
+
+print("input Date of birth is: ",A)
+print("input Month of birth is: ",C)
+print("input  of year of birth is: ",B)
